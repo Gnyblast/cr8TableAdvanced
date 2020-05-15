f577b74420c9a9de851e113d4090d745c1159e60
Güney Saramali <guneysaramali@live.co.uk>
2020-05-15 22:37:35 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,34 +1,56 @@
-var tableConfig =  {
-    headers:  ["ID",  "Username",  "Name",  "Phone",  "Email",  "Active",  "Accountant",  "Admin",  "Date"],
-    assocNames:["id",  "username",  "name",  "phone",  "email",  "isactive",  "isaccountant",  "isadmin",  "date"],
-    isAngularAppExist:  false,
-    angularAppName:  "cr8App",
-    newAngularContollerName:  "cr8Control",
-    data:  "myData",
-    searchModule:  true,
-    advancedSearch:  true,
-    pageLimiter:  true,
-    pageLimitOptions:  [2,5,10,20,50,100],
-    pageLimitDefault:  2,
-    classes:  {
-	    tableClasses:  "my-table center-table",
-	    theadClasses:  "my-thead center-thead",
-	    theadTrClasses:  "my-thead-tr text-center",
-	    theadThClasses:  "my-th center-text",
-	    theadTdClasses:  "my-td-th same-width",
-	    searchInputClasses:  "padding-class text-bold",
-	    searchComboBoxClasses:  "my-select text-bold",
-	    pageLimitClasses:  "my-select text-bold",
-	    tbodyClasses:  "my-tbody center-tbody",
-	    tbodyTrClasses:  "my-tr center-text",
-	    tbodyTdClasses:  "bold-text center-text",
-	    pageNumbersClasses:  "my-page-numbers",
-	    paginateArrowClasses:  "paginate-class",
-	    activePageNumberClass:"active-page-num"
-    }
-}
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="/css/home.css<?php include('vers.html'); ?>" type="text/css">
+		<link rel="stylesheet" href="/css/sideBar.css<?php include('vers.html'); ?>" type="text/css">
+		<script type="module">
+			import { Cr8TableBuilder } from '/js/cr8table.js';
+			var tableConfig =  {
+				headers:  ["ürün",  "Kategori",  "Adet",  "Fiyat",  "Tarih"],
+				assocNames:["Product",  "Category",  "Piece",  "Cost",  "Date"],
+				isAngularAppExist:  true,
+				angularAppName:  "myApp",
+				newAngularContollerName:  "cr8Control",
+				data:  "stockData",
+				isDataFromParentAngular: true,
+				searchModule:  true,
+				advancedSearch:  true,
+				pageLimiter:  true,
+				pageLimitOptions:  [2,5,10,20,50,100],
+				pageLimitDefault:  2,
+				classes:  {
+					tableClasses:  "my-table center-table",
+					theadClasses:  "my-thead center-thead",
+					theadTrClasses:  "my-thead-tr text-center",
+					theadThClasses:  "my-th center-text",
+					theadTdClasses:  "my-td-th same-width",
+					searchInputClasses:  "padding-class text-bold",
+					searchComboBoxClasses:  "my-select text-bold",
+					pageLimitClasses:  "my-select text-bold",
+					tbodyClasses:  "my-tbody center-tbody",
+					tbodyTrClasses:  "my-tr center-text",
+					tbodyTdClasses:  "bold-text center-text",
+					pageNumbersClasses:  "my-page-numbers",
+					paginateArrowClasses:  "paginate-class",
+					activePageNumberClass:"active-page-num"
+				}
+			}
 
-var tableBuilder =  new  Cr8TableBuilder(tableConfig);
-var table = tableBuilder.run();
-document.getElementById('app').appendChild(table);
-tableBuilder.angularStart();
+			var tableBuilder =  new  Cr8TableBuilder(tableConfig);
+			var table = tableBuilder.run();
+			document.getElementById('container').appendChild(table);
+			tableBuilder.angularStart();
+		</script>
+		<script src="/angular.min.js"></script>
+		<script src="/js/test.js<?php include('vers.html'); ?>"></script>
+		<meta charset="utf-8">
+		<title>Some title</title>
+	</head>
+
+	<body ng-app="myApp" ng-controller="myCtrl" ng-cloak>
+		<div id="container">
+		</div>
+	</body>
+</html>
